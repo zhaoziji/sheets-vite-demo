139adef81f3f8b893fe43a81f5ad5438f741d069
白熱 <sonne@asaki.me>
2024-02-24 16:17:56 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import "@univerjs/sheets-numfmt/lib/index.css";
 import { LocaleType, LogLevel, Univer } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
 import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
 import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
 import { UniverSheetsPlugin } from "@univerjs/sheets";
@@ -27,6 +28,7 @@ const univer = new Univer({
 univer.registerPlugin(UniverDocsPlugin, {
   hasScroll: false,
 });
+univer.registerPlugin(UniverDocsUIPlugin);
 univer.registerPlugin(UniverRenderEnginePlugin);
 univer.registerPlugin(UniverUIPlugin, {
   container: "app",
