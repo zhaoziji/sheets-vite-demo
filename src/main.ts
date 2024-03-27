ae412b6def80d4db475261dd2ad60f15679a3812
白熱 <sonne@asaki.me>
2024-03-27 11:52:04 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import "@univerjs/sheets-ui/lib/index.css";
 import "@univerjs/sheets-formula/lib/index.css";
 import "@univerjs/sheets-numfmt/lib/index.css";
 
-import { LocaleType, LogLevel, Univer } from "@univerjs/core";
+import { Univer } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
 import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
@@ -13,35 +13,29 @@ import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
 import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
 import { UniverSheetsPlugin } from "@univerjs/sheets";
 import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
-import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
 import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
 import { UniverUIPlugin } from "@univerjs/ui";
 
-// univer
 const univer = new Univer({
   theme: defaultTheme,
-  locale: LocaleType.ZH_CN,
-  logLevel: LogLevel.VERBOSE,
 });
 
-// core plugins
+univer.registerPlugin(UniverRenderEnginePlugin);
+univer.registerPlugin(UniverFormulaEnginePlugin);
+
+univer.registerPlugin(UniverUIPlugin, {
+  container: 'app',
+  header: true,
+  footer: true,
+});
+
 univer.registerPlugin(UniverDocsPlugin, {
   hasScroll: false,
 });
 univer.registerPlugin(UniverDocsUIPlugin);
-univer.registerPlugin(UniverRenderEnginePlugin);
-univer.registerPlugin(UniverUIPlugin, {
-  container: "app",
-  header: true,
-  toolbar: true,
-  footer: true,
-});
+
 univer.registerPlugin(UniverSheetsPlugin);
 univer.registerPlugin(UniverSheetsUIPlugin);
-
-// sheet feature plugins
-univer.registerPlugin(UniverSheetsNumfmtPlugin);
-univer.registerPlugin(UniverFormulaEnginePlugin);
 univer.registerPlugin(UniverSheetsFormulaPlugin);
 
 // create univer sheet instance
